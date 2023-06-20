1411d4eb7697e4111bd55f2b2c128c5cf3d56b44
PetrDYu <68391651+PetrDYu@users.noreply.github.com>
2023-06-20 22:42:51 +0300

    Increased padding below song text to avoid overlapping floating buttons

--- app/src/main/java/ru/petr/songapp/ui/screens/songScreens/songViewerScreen/SongViewerHandler.kt
+++ app/src/main/java/ru/petr/songapp/ui/screens/songScreens/songViewerScreen/SongViewerHandler.kt
@@ -164,7 +164,7 @@ fun SongViewer(modifier: Modifier, song: Song?, fontSize: Int) {
             SongView(modifier = Modifier.padding(start = 10.dp,
                                                  end = 20.dp,
                                                  top = 30.dp,
-                                                 bottom=30.dp),
+                                                 bottom=100.dp),
                      showType = SongShowTypes.VIEW,
                      song = song,
                      fontSize = fontSize)
